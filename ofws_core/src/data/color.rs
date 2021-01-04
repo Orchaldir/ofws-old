3105c2e2e9a5ac23e8a2182b908f66ef14d1d0be
Orchaldir <orchaldir@gmail.com>
2021-01-04 21:25:07 +0100

    [#4] Fix lint

--- ofws_core/src/data/color.rs
+++ ofws_core/src/data/color.rs
@@ -45,7 +45,7 @@ impl Color {
     }
 }
 
-pub const BLACK: Color = Color::new(0,0,0);
+pub const BLACK: Color = Color::new(0, 0, 0);
 pub const BLUE: Color = Color::new(0, 0, 255);
 pub const CYAN: Color = Color::new(0, 255, 255);
 pub const GREEN: Color = Color::new(0, 255, 0);
