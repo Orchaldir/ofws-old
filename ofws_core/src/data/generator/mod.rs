0dc2b106ef14aa40596c891eb52909cd987beb64
Orchaldir <orchaldir@gmail.com>
2021-01-19 23:28:05 +0100

    [#16] Add MockGenerator

--- ofws_core/src/data/generator/mod.rs
+++ ofws_core/src/data/generator/mod.rs
@@ -5,7 +5,7 @@ pub trait Generator {
     fn generate(&self, x: u32, y: u32) -> u8;
 }
 
-/// Returns the same value for all points.
+/// Returns the same value for all 2d points.
 pub struct ConstantValue {
     value: u8,
 }
@@ -17,7 +17,7 @@ impl ConstantValue {
 }
 
 impl Generator for ConstantValue {
-    /// Returns the same value for all points.
+    /// Returns the same value for all 2d points.
     ///
     /// ```
     ///# use ofws_core::data::generator::{ConstantValue, Generator};
@@ -29,5 +29,39 @@ impl Generator for ConstantValue {
     /// ```
     fn generate(&self, _x: u32, _y: u32) -> u8 {
         self.value
+    }
+}
+
+/// Returns a value for a specific 2d point.
+pub struct MockGenerator {
+    x: u32,
+    y: u32,
+    value: u8,
+}
+
+impl MockGenerator {
+    pub fn new(x: u32, y: u32, value: u8) -> MockGenerator {
+        MockGenerator { x, y, value }
+    }
+}
+
+impl Generator for MockGenerator {
+    /// Returns a value for a specific 2d point.
+    ///
+    /// ```
+    ///# use ofws_core::data::generator::{Generator, MockGenerator};
+    /// let generator = MockGenerator::new(3, 4, 42);
+    /// assert_eq!(generator.generate(0, 0), 0);
+    /// assert_eq!(generator.generate(10, 0), 0);
+    /// assert_eq!(generator.generate(0, 20), 0);
+    /// assert_eq!(generator.generate(123, 345), 0);
+    /// assert_eq!(generator.generate(3, 4), 42);
+    /// ```
+    fn generate(&self, x: u32, y: u32) -> u8 {
+        if self.x == x && self.y == y {
+            self.value
+        } else {
+            0
+        }
     }
 }
