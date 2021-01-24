fd4a4190a6a153c9fd3f933736e77f4a93e90e6f
Orchaldir <orchaldir@gmail.com>
2021-01-24 19:51:29 +0100

    [#6] Finish Map2d

--- ofws_core/src/data/map/mod.rs
+++ ofws_core/src/data/map/mod.rs
@@ -24,7 +24,6 @@ impl Map2d {
     /// Adds a new [`Attribute`] to the map and resturns its id.
     ///
     /// ```
-    ///# use ofws_core::data::map::attribute::Attribute;
     ///# use ofws_core::data::map::Map2d;
     ///# use ofws_core::data::size2d::Size2d;
     /// let mut map = Map2d::new(Size2d::new(2, 3));
@@ -36,7 +35,6 @@ impl Map2d {
     /// Fails if the map already contains an [`Attribute`] with the same name.
     ///
     /// ```
-    ///# use ofws_core::data::map::attribute::Attribute;
     ///# use ofws_core::data::map::Map2d;
     ///# use ofws_core::data::size2d::Size2d;
     /// let mut map = Map2d::new(Size2d::new(2, 3));
@@ -58,10 +56,26 @@ impl Map2d {
         Some(id)
     }
 
+    /// Returns the id of the [`Attribute`] with the matching name.
+    ///
+    /// ```
+    ///# use ofws_core::data::map::Map2d;
+    ///# use ofws_core::data::size2d::Size2d;
+    /// let mut map = Map2d::new(Size2d::new(2, 3));
+    /// map.create_attribute("elevation", 42);
+    /// map.create_attribute("rainfall", 100);
+    ///
+    /// assert_eq!(map.get_attribute_id("elevation"), Some(0));
+    /// assert_eq!(map.get_attribute_id("rainfall"), Some(1));
+    /// assert_eq!(map.get_attribute_id("unknown"), None);
+    /// ```
+    pub fn get_attribute_id<S: Into<String>>(&self, name: S) -> Option<usize> {
+        self.attribute_lookup.get(&name.into()).copied()
+    }
+
     /// Returns an [`Attribute`] with the matching id.
     ///
     /// ```
-    ///# use ofws_core::data::map::attribute::Attribute;
     ///# use ofws_core::data::map::Map2d;
     ///# use ofws_core::data::size2d::Size2d;
     /// let mut map = Map2d::new(Size2d::new(2, 3));
@@ -77,7 +91,6 @@ impl Map2d {
     /// Panics if there is no matching id.
     ///
     /// ```should_panic
-    ///# use ofws_core::data::map::attribute::Attribute;
     ///# use ofws_core::data::map::Map2d;
     ///# use ofws_core::data::size2d::Size2d;
     /// let mut map = Map2d::new(Size2d::new(2, 3));
@@ -86,5 +99,34 @@ impl Map2d {
     /// ```
     pub fn get_attribute(&self, id: usize) -> &Attribute {
         unwrap!(self.attributes.get(id), "Unknown attribute id {}!", id)
+    }
+
+    /// Returns a mutable [`Attribute`] with the matching id.
+    ///
+    /// ```
+    ///# use ofws_core::data::map::attribute::Attribute;
+    ///# use ofws_core::data::map::Map2d;
+    ///# use ofws_core::data::size2d::Size2d;
+    /// let mut map = Map2d::new(Size2d::new(2, 3));
+    /// map.create_attribute("elevation", 42);
+    /// map.create_attribute("rainfall", 100);
+    ///
+    /// assert_eq!(map.get_attribute_mut(0).get_name(), "elevation");
+    /// assert_eq!(map.get_attribute_mut(1).get_name(), "rainfall");
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// Panics if there is no matching id.
+    ///
+    /// ```should_panic
+    ///# use ofws_core::data::map::Map2d;
+    ///# use ofws_core::data::size2d::Size2d;
+    /// let mut map = Map2d::new(Size2d::new(2, 3));
+    ///
+    /// map.get_attribute_mut(0);
+    /// ```
+    pub fn get_attribute_mut(&mut self, id: usize) -> &mut Attribute {
+        unwrap!(self.attributes.get_mut(id), "Unknown attribute id {}!", id)
     }
 }
