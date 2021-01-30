9005b8736793f1de129e3d1858fd560269ba9bb1
Orchaldir <orchaldir@gmail.com>
2021-01-30 10:59:28 +0100

    [#17] Improve VectorInterpolator

--- ofws_core/src/data/math/interpolation/vector.rs
+++ ofws_core/src/data/math/interpolation/vector.rs
@@ -2,11 +2,11 @@ use crate::data::math::interpolation::{Interpolate, Interpolator};
 
 /// Interpolates multiple elements. Each element is paired with its position between 0.0 & 1.0.
 #[derive(Debug, PartialEq)]
-pub struct VectorInterpolator<T: Interpolate> {
-    elements: Vec<(f32, T)>,
+pub struct VectorInterpolator<T: Clone + Interpolate> {
+    vector: Vec<(f32, T)>,
 }
 
-impl<T: Interpolate> VectorInterpolator<T> {
+impl<T: Clone + Interpolate> VectorInterpolator<T> {
     /// Returns a VectorInterpolator, if the input is valid:
     ///
     /// ```
@@ -35,14 +35,14 @@ impl<T: Interpolate> VectorInterpolator<T> {
     /// assert!(VectorInterpolator::new(vec![(-0.5,50),(0.8,200)]).is_none());
     /// assert!(VectorInterpolator::new(vec![(0.5,50),(1.2,200)]).is_none());
     /// ```
-    pub fn new(elements: Vec<(f32, T)>) -> Option<VectorInterpolator<T>> {
-        if elements.len() < 2 {
+    pub fn new(vector: Vec<(f32, T)>) -> Option<VectorInterpolator<T>> {
+        if vector.len() < 2 {
             return None;
         }
 
         let mut last_value = 0.0;
 
-        for (value, _) in &elements {
+        for (value, _) in &vector {
             if *value < last_value {
                 return None;
             }
@@ -53,11 +53,11 @@ impl<T: Interpolate> VectorInterpolator<T> {
             return None;
         }
 
-        Some(VectorInterpolator { elements })
+        Some(VectorInterpolator { vector })
     }
 }
 
-impl<T: Interpolate> Interpolator<T> for VectorInterpolator<T> {
+impl<T: Clone + Interpolate> Interpolator<T> for VectorInterpolator<T> {
     /// Returns the interpolated value.
     ///
     /// ```
@@ -75,21 +75,21 @@ impl<T: Interpolate> Interpolator<T> for VectorInterpolator<T> {
     /// assert_eq!(interpolator.interpolate(1.0), 200);
     /// ```
     fn interpolate(&self, factor: f32) -> T {
-        let mut last_entry = self.elements.get(0).unwrap();
+        let mut last_entry = self.vector.get(0).unwrap();
 
         if factor <= last_entry.0 {
-            return last_entry.1.lerp(&last_entry.1, 0.0);
+            return last_entry.1.clone();
         }
 
-        for entry in &self.elements[1..] {
+        for entry in &self.vector[1..] {
             if factor <= entry.0 {
-                let factor = (factor - last_entry.0) / (entry.0 - last_entry.0);
-                return last_entry.1.lerp(&entry.1, factor);
+                let factor_in_interval = (factor - last_entry.0) / (entry.0 - last_entry.0);
+                return last_entry.1.lerp(&entry.1, factor_in_interval);
             }
 
             last_entry = entry;
         }
 
-        last_entry.1.lerp(&last_entry.1, 0.0)
+        last_entry.1.clone()
     }
 }
