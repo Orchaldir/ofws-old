eeb7ad797905db3c97045d2e3e01537dcc4c9a8f
Orchaldir <orchaldir@gmail.com>
2021-02-06 22:44:45 +0100

    [#25] Add Distortion2d

--- ofws_core/src/data/map/generation/distortion.rs
+++ ofws_core/src/data/map/generation/distortion.rs
@@ -1,4 +1,5 @@
 use crate::data::generator1d::Generator1d;
+use crate::data::generator2d::Generator2d;
 use crate::data::map::attribute::Attribute;
 use crate::data::map::generation::GenerationStep;
 use crate::data::map::Map2d;
@@ -39,6 +40,7 @@ impl DistortAlongX {
 
         for y in 0..map.size.height() {
             let shift = self.generator.generate(y);
+            debug!("y={} shift={}", y, shift);
             self.distort_row(y, shift, attribute, &mut values);
         }
 
@@ -123,7 +125,7 @@ impl DistortAlongY {
 
         for x in 0..map.size.width() {
             let shift = self.generator.generate(x);
-            info!("x={} shift={}", x, shift);
+            debug!("x={} shift={}", x, shift);
             self.distort_column(x, shift, attribute, &mut values);
         }
 
@@ -155,6 +157,62 @@ impl GenerationStep for DistortAlongY {
     fn execute(&self, map: &mut Map2d) {
         info!(
             "Distort attribute '{}' of map '{}' along the y-axis.",
+            map.get_attribute(self.attribute_id).get_name(),
+            map.get_name()
+        );
+
+        let values = self.distort_map(map);
+        let attribute = map.get_attribute_mut(self.attribute_id);
+
+        attribute.replace_values(values);
+    }
+}
+
+/// Distorts an [`Attribute`] along 2 dimensions.
+pub struct Distortion2d {
+    attribute_id: usize,
+    generator_x: Box<dyn Generator2d>,
+    generator_y: Box<dyn Generator2d>,
+}
+
+impl Distortion2d {
+    pub fn new(
+        attribute_id: usize,
+        generator_x: Box<dyn Generator2d>,
+        generator_y: Box<dyn Generator2d>,
+    ) -> Distortion2d {
+        Distortion2d {
+            attribute_id,
+            generator_x,
+            generator_y,
+        }
+    }
+
+    fn distort_map(&self, map: &Map2d) -> Vec<u8> {
+        let length = map.size.get_area();
+        let attribute = map.get_attribute(self.attribute_id);
+        let mut values = Vec::with_capacity(length);
+
+        for y in 0..map.size.height() {
+            for x in 0..map.size.width() {
+                let shift_x = self.generator_x.generate(x, y) as u32;
+                let shift_y = self.generator_y.generate(x, y) as u32;
+                let distorted_x = x + shift_x;
+                let distorted_y = y + shift_y;
+                let index = map.size.saturating_to_index(distorted_x, distorted_y);
+                values.push(attribute.get(index));
+            }
+        }
+
+        values
+    }
+}
+
+impl GenerationStep for Distortion2d {
+    // Executes the step.
+    fn execute(&self, map: &mut Map2d) {
+        info!(
+            "Distort attribute '{}' of map '{}' in 2 dimensions.",
             map.get_attribute(self.attribute_id).get_name(),
             map.get_name()
         );
