090adaeb108698a639681093c396e24d11355747
Orchaldir <orchaldir@gmail.com>
2021-02-10 21:48:27 +0100

    [#26] Rename to Noise1d

--- ofws_core/src/data/generator/generator1d.rs
+++ ofws_core/src/data/generator/generator1d.rs
@@ -5,7 +5,7 @@ pub enum Generator1d {
     /// Returns the input as output.
     InputAsOutput,
     /// Generates values with Super Simplex noise.
-    NoiseGenerator1d {
+    Noise1d {
         algo: SuperSimplex,
         seed: u32,
         scale: f64,
@@ -15,7 +15,7 @@ pub enum Generator1d {
 
 impl Generator1d {
     pub fn new_noise(seed: u32, scale: f64, max_value: u8) -> Generator1d {
-        Generator1d::NoiseGenerator1d {
+        Generator1d::Noise1d {
             algo: SuperSimplex::new().set_seed(seed),
             seed,
             scale,
@@ -27,7 +27,7 @@ impl Generator1d {
     pub fn generate(&self, input: u32) -> u8 {
         match self {
             Generator1d::InputAsOutput => input as u8,
-            Generator1d::NoiseGenerator1d {
+            Generator1d::Noise1d {
                 algo,
                 seed: _,
                 scale,
