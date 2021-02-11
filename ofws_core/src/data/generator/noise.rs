f403b104132b0ebb9995f9d272b8b2a806edf75f
Orchaldir <orchaldir@gmail.com>
2021-02-11 19:40:43 +0100

    [#26] Add NoiseData

--- ofws_core/src/data/generator/noise.rs
+++ ofws_core/src/data/generator/noise.rs
@@ -1,5 +1,7 @@
 use noise::{NoiseFn, Seedable, SuperSimplex};
+use std::convert::TryFrom;
 
+/// Hide the noise functions from [`noise`].
 pub struct Noise {
     algo: Box<SuperSimplex>,
     scale: f64,
@@ -7,12 +9,17 @@ pub struct Noise {
 }
 
 impl Noise {
-    pub fn new(seed: u32, scale: f64, max_value: u8) -> Noise {
-        Noise {
+    /// Try to create a Noise.
+    pub fn new(seed: u32, scale: f64, max_value: u8) -> Result<Noise, &'static str> {
+        if scale <= 0.0 {
+            return Err("Noise's scale must be positive!");
+        }
+
+        Ok(Noise {
             algo: Box::new(SuperSimplex::new().set_seed(seed)),
             scale,
             factor: max_value as f64 / 2.0,
-        }
+        })
     }
 
     /// Generates noise for an input.
@@ -28,5 +35,41 @@ impl Noise {
         let y = y as f64 / self.scale;
         let positive_value = self.algo.get([x, y]) + 1.0;
         (positive_value * self.factor) as u8
+    }
+}
+
+/// For serializing, deserializing & validating [`Noise`].
+///
+///```
+///# use ofws_core::data::generator::noise::{NoiseData, Noise};
+/// use std::convert::TryInto;
+///
+/// let data = NoiseData { seed: 300, scale: 5, max_value: 128 };
+/// let noise: Noise = data.clone().try_into().unwrap();
+/// let result: NoiseData = noise.into();
+/// assert_eq!(data, result)
+///```
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct NoiseData {
+    pub seed: u32,
+    pub scale: u32,
+    pub max_value: u8,
+}
+
+impl TryFrom<NoiseData> for Noise {
+    type Error = &'static str;
+
+    fn try_from(data: NoiseData) -> Result<Self, Self::Error> {
+        Noise::new(data.seed, data.scale as f64, data.max_value)
+    }
+}
+
+impl From<Noise> for NoiseData {
+    fn from(noise: Noise) -> Self {
+        NoiseData {
+            seed: noise.algo.seed(),
+            scale: noise.scale as u32,
+            max_value: (noise.factor * 2.0) as u8,
+        }
     }
 }
