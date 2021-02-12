9fe0a457e27dcd388816645cc9e1511eb3d8844c
Orchaldir <orchaldir@gmail.com>
2021-02-12 17:16:53 +0100

    [#26] Add Selector::InterpolatePair

--- ofws_core/src/data/selector.rs
+++ ofws_core/src/data/selector.rs
@@ -1,11 +1,21 @@
+use crate::data::math::interpolation::Interpolate;
 use std::collections::HashMap;
 
-pub trait Selection: Default + Clone + Copy {}
+pub trait Selection: Default + Interpolate + Clone + Copy {}
 
 impl Selection for u8 {}
 
 /// Selects an object of type T based on the input.
 pub enum Selector<T: Selection> {
+    /// Interpolates 2 elements.
+    ///
+    /// ```
+    ///# use ofws_core::data::selector::Selector;
+    /// let selector = Selector::new_interpolate_pair(100, 200);
+    ///
+    /// assert_eq!(selector.get(128), 150);
+    /// ```
+    InterpolatePair { first: T, second: T },
     /// Looks the input up in a hashmap or returns the default value.
     ///
     /// ```
@@ -23,9 +33,16 @@ pub enum Selector<T: Selection> {
 }
 
 impl<T: Selection> Selector<T> {
+    pub fn new_interpolate_pair(first: T, second: T) -> Selector<T> {
+        Selector::InterpolatePair { first, second }
+    }
+
     /// Selects an object of type T based on the input.
     pub fn get(&self, input: u8) -> T {
         match self {
+            Selector::InterpolatePair { first, second } => {
+                first.lerp(&second, input as f32 / 255.0)
+            }
             Selector::Lookup(hashmap) => hashmap.get(&input).copied().unwrap_or_else(T::default),
         }
     }
