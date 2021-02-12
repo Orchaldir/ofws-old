a7de973b86d965b8ada6b4156c54b362432eec5b
Orchaldir <orchaldir@gmail.com>
2021-02-12 17:32:18 +0100

    [#26] Add Selector::InterpolateVector

--- ofws_core/src/data/selector.rs
+++ ofws_core/src/data/selector.rs
@@ -16,6 +16,22 @@ pub enum Selector<T: Selection> {
     /// assert_eq!(selector.get(128), 150);
     /// ```
     InterpolatePair { first: T, second: T },
+    /// Interpolates multiple elements.
+    ///
+    /// ```
+    ///# use ofws_core::data::selector::Selector;
+    /// let interpolator = Selector::InterpolateVector(vec![(100,150), (150,200), (200, 100)]);
+    ///
+    /// assert_eq!(interpolator.get(  0), 150);
+    /// assert_eq!(interpolator.get( 50), 150);
+    /// assert_eq!(interpolator.get(100), 150);
+    /// assert_eq!(interpolator.get(125), 175);
+    /// assert_eq!(interpolator.get(150), 200);
+    /// assert_eq!(interpolator.get(175), 150);
+    /// assert_eq!(interpolator.get(200), 100);
+    /// assert_eq!(interpolator.get(255), 100);
+    /// ```
+    InterpolateVector(Vec<(u8, T)>),
     /// Looks the input up in a hashmap or returns the default value.
     ///
     /// ```
@@ -40,10 +56,31 @@ impl<T: Selection> Selector<T> {
     /// Selects an object of type T based on the input.
     pub fn get(&self, input: u8) -> T {
         match self {
+            Selector::InterpolateVector(vector) => interpolate(vector, input),
             Selector::InterpolatePair { first, second } => {
                 first.lerp(&second, input as f32 / 255.0)
             }
             Selector::Lookup(hashmap) => hashmap.get(&input).copied().unwrap_or_else(T::default),
         }
     }
+}
+
+fn interpolate<T: Selection>(vector: &[(u8, T)], input: u8) -> T {
+    let mut last_entry = vector.get(0).unwrap();
+
+    if input <= last_entry.0 {
+        return last_entry.1;
+    }
+
+    for entry in &vector[1..] {
+        if input <= entry.0 {
+            let factor_in_interval =
+                (input - last_entry.0) as f32 / (entry.0 - last_entry.0) as f32;
+            return last_entry.1.lerp(&entry.1, factor_in_interval);
+        }
+
+        last_entry = entry;
+    }
+
+    last_entry.1
 }
