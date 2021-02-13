9f292374ab680b92b9882650d3490868e8f195d3
Orchaldir <orchaldir@gmail.com>
2021-02-13 16:26:53 +0100

    [#26] Imprve clusterer2d::new()

--- ofws_core/src/data/math/transformer/clusterer2d.rs
+++ ofws_core/src/data/math/transformer/clusterer2d.rs
@@ -10,15 +10,31 @@ pub struct Clusterer2d {
 }
 
 impl Clusterer2d {
-    pub fn new(size: Size2d, cluster_id_lookup: Vec<u8>) -> Clusterer2d {
+    /// Returns a clusterer, if valid:
+    ///
+    /// ```
+    ///# use ofws_core::data::math::size2d::Size2d;
+    ///# use ofws_core::data::math::transformer::clusterer2d::Clusterer2d;
+    /// assert!(Clusterer2d::new(Size2d::new(2,  2), vec![10, 20, 30, 40, 50, 60]).is_err());
+    /// assert!(Clusterer2d::new(Size2d::new(2, 10), vec![10, 20, 30, 40, 50, 60]).is_err());
+    /// assert!(Clusterer2d::new(Size2d::new(0,  0), vec![10, 20, 30, 40, 50, 60]).is_err());
+    /// assert!(Clusterer2d::new(Size2d::new(0,  0), vec![]).is_err());
+    /// ```
+    pub fn new(size: Size2d, cluster_id_lookup: Vec<u8>) -> Result<Clusterer2d, &'static str> {
+        if size.get_area() != cluster_id_lookup.len() {
+            return Err("Size & look up table don't match!");
+        } else if cluster_id_lookup.len() < 2 {
+            return Err("Needs more than 1 cluster!");
+        }
+
         let width = calculate_cluster_size(size.width());
         let height = calculate_cluster_size(size.height());
 
-        Clusterer2d {
+        Ok(Clusterer2d {
             lookup_table_size: size,
             cluster_size: Size2d::new(width, height),
             cluster_id_lookup,
-        }
+        })
     }
 
     /// Calculates the cluster of 2 inputs.
@@ -26,7 +42,7 @@ impl Clusterer2d {
     /// ```
     ///# use ofws_core::data::math::size2d::Size2d;
     ///# use ofws_core::data::math::transformer::clusterer2d::Clusterer2d;
-    /// let clusterer = Clusterer2d::new(Size2d::new(3, 2), vec![10, 20, 30, 40, 50, 60]);
+    /// let clusterer = Clusterer2d::new(Size2d::new(3, 2), vec![10, 20, 30, 40, 50, 60]).unwrap();
     ///
     /// assert_eq!(clusterer.cluster(  0,   0), 10);
     /// assert_eq!(clusterer.cluster(100,  60), 20);
@@ -42,7 +58,7 @@ impl Clusterer2d {
 
         *self.cluster_id_lookup.get(index).unwrap_or_else(|| {
             panic!(
-                "Index {} is to large for {} clusters!",
+                "Index {} is too large for {} clusters!",
                 index,
                 self.cluster_id_lookup.len()
             )
