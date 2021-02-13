f83494fd6d1b752c87b9dec9bed69e84c52cc754
Orchaldir <orchaldir@gmail.com>
2021-02-13 19:50:43 +0100

    [#26] Extract step.rs

--- ofws_core/src/data/map/generation/mod.rs
+++ ofws_core/src/data/map/generation/mod.rs
@@ -1,9 +1,4 @@
-use crate::data::map::generation::attribute::CreateAttribute;
-use crate::data::map::generation::distortion::distortion2d::Distortion2d;
-use crate::data::map::generation::distortion::Distortion1d;
-use crate::data::map::generation::generator::GeneratorStep;
-use crate::data::map::generation::modify::ModifyWithAttribute;
-use crate::data::map::generation::transformer::TransformAttribute2d;
+use crate::data::map::generation::step::GenerationStep;
 use crate::data::map::Map2d;
 use crate::data::math::size2d::Size2d;
 use std::ops::Sub;
@@ -12,6 +7,7 @@ pub mod attribute;
 pub mod distortion;
 pub mod generator;
 pub mod modify;
+pub mod step;
 pub mod transformer;
 
 /// Generates a map based on a number of steps.
@@ -62,33 +58,5 @@ impl MapGeneration {
         info!("Finished generation of '{}' in {:?}", self.name, duration);
 
         map
-    }
-}
-
-/// A step during map generation.
-pub enum GenerationStep {
-    CreateAttribute(CreateAttribute),
-    DistortAlongX(Distortion1d),
-    DistortAlongY(Distortion1d),
-    Distortion2d(Distortion2d),
-    GeneratorAdd(GeneratorStep),
-    GeneratorSub(GeneratorStep),
-    ModifyWithAttribute(ModifyWithAttribute),
-    TransformAttribute2d(TransformAttribute2d),
-}
-
-impl GenerationStep {
-    /// Runs the step.
-    pub fn run(&self, map: &mut Map2d) {
-        match self {
-            GenerationStep::CreateAttribute(step) => step.run(map),
-            GenerationStep::DistortAlongX(step) => step.distort_along_x(map),
-            GenerationStep::DistortAlongY(step) => step.distort_along_y(map),
-            GenerationStep::Distortion2d(step) => step.run(map),
-            GenerationStep::GeneratorAdd(step) => step.add(map),
-            GenerationStep::GeneratorSub(step) => step.sub(map),
-            GenerationStep::ModifyWithAttribute(step) => step.run(map),
-            GenerationStep::TransformAttribute2d(step) => step.run(map),
-        }
     }
 }
