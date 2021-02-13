7628b818de91aca58e9ce5d8987d51521708e08d
Orchaldir <orchaldir@gmail.com>
2021-02-13 08:08:16 +0100

    [#26] Add MapGeneration

--- ofws_core/src/data/map/generation/mod.rs
+++ ofws_core/src/data/map/generation/mod.rs
@@ -4,6 +4,7 @@ use crate::data::map::generation::distortion::{Distortion1d, Distortion2d};
 use crate::data::map::generation::generator::GeneratorStep;
 use crate::data::map::generation::modify::ModifyWithAttribute;
 use crate::data::map::Map2d;
+use crate::data::size2d::Size2d;
 
 pub mod attribute;
 pub mod biome;
@@ -11,7 +12,46 @@ pub mod distortion;
 pub mod generator;
 pub mod modify;
 
-/// A trait to handle a step of the map generation.
+/// Generates a map based on a number of steps.
+pub struct MapGeneration {
+    name: String,
+    size: Size2d,
+    steps: Vec<GenerationStep>,
+}
+
+impl MapGeneration {
+    pub fn new<S: Into<String>>(
+        name: S,
+        size: Size2d,
+        steps: Vec<GenerationStep>,
+    ) -> MapGeneration {
+        MapGeneration {
+            name: name.into(),
+            size,
+            steps,
+        }
+    }
+
+    /// Generates the map.
+    pub fn generate(&self) -> Map2d {
+        info!(
+            "Generate the map '{}' with {:?} in {} steps:",
+            self.name,
+            self.size,
+            self.steps.len(),
+        );
+
+        let mut map = Map2d::with_name(self.name.clone(), self.size);
+
+        self.steps.iter().for_each(|step| step.run(&mut map));
+
+        info!("Finished generation of '{}'", self.name);
+
+        map
+    }
+}
+
+/// A step during map generation.
 pub enum GenerationStep {
     CreateAttribute(CreateAttribute),
     BiomeSelector(BiomeSelector),
