542f2323c45f44a1e317b74c6ec58e81569c02bb
Orchaldir <orchaldir@gmail.com>
2021-02-15 21:59:34 +0100

    [#26] Add Clusterer2dData for missing validation & simplification

--- ofws_core/src/data/math/transformer/clusterer2d.rs
+++ ofws_core/src/data/math/transformer/clusterer2d.rs
@@ -1,5 +1,6 @@
 use crate::data::math::size2d::Size2d;
 use serde::{Deserialize, Serialize};
+use std::convert::{TryFrom, TryInto};
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Clusterer2dError {
@@ -8,7 +9,7 @@ pub enum Clusterer2dError {
 }
 
 /// Determines a cluster id from both inputs. E.g. biome from rainfall & temperature.
-#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Clusterer2d {
     lookup_table_size: Size2d,
     cluster_size: Size2d,
@@ -23,7 +24,6 @@ impl Clusterer2d {
     ///# use ofws_core::data::math::transformer::clusterer2d::Clusterer2d;
     ///# use ofws_core::data::math::transformer::clusterer2d::Clusterer2dError::{TooFewClusters, SizeMismatch};
     /// assert_eq!(Clusterer2d::new(Size2d::new(2,  2), vec![10, 20]), Err(SizeMismatch(4, 2)));
-    /// assert_eq!(Clusterer2d::new(Size2d::new(2, 10), vec![10, 20]), Err(SizeMismatch(20, 2)));
     /// assert_eq!(Clusterer2d::new(Size2d::new(0,  0), vec![10, 20]), Err(SizeMismatch(0, 2)));
     /// assert_eq!(Clusterer2d::new(Size2d::new(0,  0), vec![]), Err(TooFewClusters(0)));
     /// ```
@@ -78,4 +78,56 @@ impl Clusterer2d {
 
 fn calculate_cluster_size(number_of_clusters: u32) -> u32 {
     (256.0 / number_of_clusters as f32).ceil() as u32
+}
+
+/// For serializing, deserializing & validating [`Clusterer2d`].
+///
+///```
+///# use ofws_core::data::math::transformer::clusterer2d::{Clusterer2dData, can_convert};
+///# use ofws_core::data::math::size2d::Size2d;
+/// can_convert(Clusterer2dData::new(Size2d::new(1, 2), vec![3, 4]))
+///```
+///
+/// It can fail:
+///
+/// ```
+///# use ofws_core::data::math::size2d::Size2d;
+///# use ofws_core::data::math::transformer::clusterer2d::{Clusterer2dData, is_error};
+///# use ofws_core::data::math::transformer::clusterer2d::Clusterer2dError::SizeMismatch;
+/// is_error(Clusterer2dData::new(Size2d::new(2, 10), vec![10, 20]), SizeMismatch(20, 2));
+/// ```
+#[derive(new, Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct Clusterer2dData {
+    size: Size2d,
+    cluster_id_lookup: Vec<u8>,
+}
+
+impl TryFrom<Clusterer2dData> for Clusterer2d {
+    type Error = Clusterer2dError;
+
+    fn try_from(data: Clusterer2dData) -> Result<Self, Self::Error> {
+        Clusterer2d::new(data.size, data.cluster_id_lookup)
+    }
+}
+
+impl From<&Clusterer2d> for Clusterer2dData {
+    fn from(clusterer: &Clusterer2d) -> Self {
+        Clusterer2dData {
+            size: clusterer.lookup_table_size,
+            cluster_id_lookup: clusterer.cluster_id_lookup.clone(),
+        }
+    }
+}
+
+pub fn can_convert(data: Clusterer2dData) {
+    let generator: Clusterer2d = data.clone().try_into().unwrap();
+    let result: Clusterer2dData = (&generator).into();
+    assert_eq!(result, data)
+}
+
+pub fn is_error(
+    data: impl TryInto<Clusterer2d, Error = Clusterer2dError>,
+    error: Clusterer2dError,
+) {
+    assert_eq!(data.try_into(), Err(error));
 }
