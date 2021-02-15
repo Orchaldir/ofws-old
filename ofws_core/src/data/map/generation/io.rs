84ca4c725e66373b2c7ba85796127ccc901fb087
Orchaldir <orchaldir@gmail.com>
2021-02-15 19:54:12 +0100

    [#28] Return result for write_map_generator()

--- ofws_core/src/data/map/generation/io.rs
+++ ofws_core/src/data/map/generation/io.rs
@@ -3,7 +3,6 @@ use std::convert::TryInto;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
-use std::path::Path;
 
 pub fn read_map_generator(path: &str) -> Result<MapGeneration, MapGenerationError> {
     let string = fs::read_to_string(path)?;
@@ -11,20 +10,13 @@ pub fn read_map_generator(path: &str) -> Result<MapGeneration, MapGenerationErro
     data.try_into()
 }
 
-pub fn write_map_generator(map_generator: &MapGeneration, path: &str) {
-    let path = Path::new(path);
-    let display = path.display();
-
-    let mut file = match File::create(&path) {
-        Err(why) => panic!("Couldn't create {}: {}", display, why),
-        Ok(file) => file,
-    };
+pub fn write_map_generator(map_generator: &MapGeneration, path: &str) -> Result<(), MapGenerationError> {
+    let mut file = File::create(path)?;
 
     let data: MapGenerationData = map_generator.into();
-    let s = serde_yaml::to_string(&data).unwrap();
+    let s = serde_yaml::to_string(&data)?;
 
-    match file.write_all(s.as_bytes()) {
-        Err(why) => panic!("Couldn't write to {}: {}", display, why),
-        Ok(_) => info!("Successfully wrote map generator to {}", display),
-    }
+    file.write_all(s.as_bytes())?;
+
+    Ok(())
 }
